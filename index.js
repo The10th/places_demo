a3419f2d9fbbcd9ae69c459abe4fdd55a6a38ff6
ericheinmiller <ericheinmiller@gmail.com>
2016-08-17 18:29:49 -0700

    added basic search functionality

--- index.js
+++ index.js
@@ -4,68 +4,75 @@ var infoWindow;
 var request;
 var service;
 var markers = [];
+var query = ['hotel'];
 
+function search(){
+  query = [document.getElementById('searchBar').value];
+  initialize();
+}
 
 function initialize() {
-    var center = new google.maps.LatLng(37.422, -122.084058);
+  var center = new google.maps.LatLng(37.422, -122.084058);
 
-    map = new google.maps.Map(document.getElementById('map'), {
-        center  : center,
-        zoom    : 13
-    });
+  map = new google.maps.Map(document.getElementById('map'), {
+    center  : center,
+    zoom    : 13
+  });
 
+  request = {
+    location    : center,
+    radius      : 8047,
+    types: query
+  };
+    console.log('asd', request.types[0]);
+
+  infoWindow = new google.maps.InfoWindow();
+
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
+
+  google.maps.event.addListener(map, 'rightclick', function(event) {
+    map.setCenter(event.latLng)
+    clearResults(markers)
     request = {
-        location    : center,
-        radius      : 8047,
-        type        : ['cafe']
+      location: event.latLng,
+      radius: 8047,
+      types: query 
     };
+    console.log('asd', request.types[0]);
 
-    infoWindow = new google.maps.InfoWindow();
-
-    service = new google.maps.places.PlacesService(map);
     service.nearbySearch(request, callback);
-
-    google.maps.event.addListner(map, 'rightclick', function(event) {
-        map.setCenter(event.latLng)
-        clearResults(markers)
-
-        var request = {
-            location: event.latLng,
-            radius: 8047,
-            types: ['cafe']
-        };
-        service.nearbySearch(request, callback);
-    })
+  })
 }
 
 function callback(results, status) {
-    if(status == google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-            markers.push(createMarker(results[i]));
-        }
+  if(status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      markers.push(createMarker(results[i]));
     }
+  }
 }
 
 function createMarker(place) {
-    var placeLoc = place.geometry.location;
-    var marker = new google.maps.Marker({
-        map     : map,
-        position: place.geometry.location
-    });
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map     : map,
+    position: place.geometry.location
+  });
 
-    google.maps.event.addListener(marker, 'click', function() {
-        infoWindow.setContent(place.name);
-        infoWindow.open(map, this);
-    });
+  google.maps.event.addListener(marker, 'click', function() {
+    infoWindow.setContent(place.name);
+    infoWindow.open(map, this);
+  });
 
-    return marker;
+  return marker;
 }
 
 function clearResults(markers) {
-    for (var m in markers) {
-        markers[m].setMap(null)
-    }
-    markers = []
+  for (var m in markers) {
+    markers[m].setMap(null)
+  }
+  markers = []
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
